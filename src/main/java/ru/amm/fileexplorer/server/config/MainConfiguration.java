0eaa6a19c57c58d2608a39ced30dad6100198044
Konstantin Nekrasov <const.nekrasoff@gmail.com>
2019-10-24 14:52:26 +0300

    Implemented example page on freemarker

---
+++ src/main/java/ru/amm/fileexplorer/server/config/MainConfiguration.java
@@ -0,0 +1,36 @@
+package ru.amm.fileexplorer.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@EnableWebMvc
+@Configuration
+public class MainConfiguration implements WebMvcConfigurer {
+    @Bean
+    public FreeMarkerViewResolver freemarkerViewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(true);
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/scripts/**")
+                .addResourceLocations("classpath:/static/scripts");
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfigurer() {
+        FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+        freeMarkerConfigurer.setTemplateLoaderPath("classpath:/templates/");
+        return freeMarkerConfigurer;
+    }
+}
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,16 @@
+<#ftl output_format="HTML">
+<!doctype html>
+<html>
+  <head>
+    <title>Hello</title>
+  </head>
+  <body>
+    <div>
+      <h1>This is just an example page</h1>
+    </div>
+    <div>
+      Username: ${username}
+      привет
+    </div>
+  </body>
+</html>
