b73280255a997e96d84eab13f47f54b35bf2c994
Konstantin Nekrasov <const.nekrasoff@gmail.com>
2019-10-24 19:05:06 +0300

    Add jquery to statics (example)

--- src/main/java/ru/amm/fileexplorer/server/config/MainConfiguration.java
+++ src/main/java/ru/amm/fileexplorer/server/config/MainConfiguration.java
@@ -1,5 +1,8 @@
 package ru.amm.fileexplorer.server.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,6 +15,7 @@ import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 @EnableWebMvc
 @Configuration
 public class MainConfiguration implements WebMvcConfigurer {
+    private final static Logger LOG = LoggerFactory.getLogger(MainConfiguration.class);
     @Bean
     public FreeMarkerViewResolver freemarkerViewResolver() {
         FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
@@ -28,7 +32,8 @@ public class MainConfiguration implements WebMvcConfigurer {
     }
 
     @Bean
-    public FreeMarkerConfigurer freeMarkerConfigurer() {
+    public FreeMarkerConfigurer freeMarkerConfigurer(@Value("${my.name}") String myName) {
+        LOG.info("My name: {}", myName);
         FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
         freeMarkerConfigurer.setTemplateLoaderPath("classpath:/templates/");
         return freeMarkerConfigurer;
