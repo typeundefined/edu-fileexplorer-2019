2fce60b6570d90d08fe732ebba8019af7dd32c99
Konstantin Nekrasov <const.nekrasoff@gmail.com>
2019-11-28 12:33:40 +0300

    admins's password is now admin for sure

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@
 --password converted by BCryptPasswordEncoder()
 
 INSERT INTO users (username, password, enabled)
-	values('admin','$2a$10$2fXjV1xaK9TCUziuNPo7BO1N5PlZFg2Rk9wek4MO74x8Oi/Yqyxiu',true);
+	values('admin','$2a$10$WUSE5n2qwdLoQrLjSP/klOrMRER9oIBbgs/CihKe3Hgo0kBqOFt42',true);
 
 INSERT INTO authorities (username, authority)
 	values('admin','ROLE_ADMIN');
