4840794097e6925f648da754f253faf2c1ccb745
Konstantin Nekrasov <konstantin.nekrasov@dsr-corporation.com>
2019-12-02 11:27:45 +0300

    Fix compile error

--- src/main/java/ru/amm/fileexplorer/server/controller/api/AuthController.java
+++ src/main/java/ru/amm/fileexplorer/server/controller/api/AuthController.java
@@ -1,23 +1,16 @@
 package ru.amm.fileexplorer.server.controller.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.amm.fileexplorer.server.data.user.UserCredentials;
-import ru.amm.fileexplorer.server.data.user.UserLoginInfo;
+import ru.amm.fileexplorer.server.data.api.UserLoginDTO;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-   // @Autowired
-    AuthenticationManager manager;
     @PostMapping
-    public void login(@RequestBody UserLoginInfo creds) {
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(creds.getUsername(), creds.getPassword());
-        manager.authenticate(token);
+    public void login(@RequestBody UserLoginDTO creds) {
+        throw new RuntimeException("Implement me");
     }
 }
